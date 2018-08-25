19ed60557c558468cf4415a9b9a632b548854b63
patelsaheb1219 <kaumik1219@gmail.com>
2018-08-25 14:42:03 +0530

    Insert and Delete Done!

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const NodeCouchdb = require('node-couchdb');
 
-// const couchdb = new NodeCouchdb({
-//     auth: {
-//         user : 'admin',
-//         password : 'admin'
-//     }
-// });
+const couchdb = new NodeCouchdb({
+    auth: {
+        user : 'admin',
+        password : 'admin'
+    }
+});
 
-// couchdb.listDatabases().then(function(dbs){
-//     console.log(dbs);
-// }); 
+const dbName = 'customers';
+const viewUrl = '_design/all_customers/_view/all';
+
+couchdb.listDatabases().then(function(dbs){
+    console.log(dbs);
+}); 
 
 const app = express();
 
@@ -23,7 +26,48 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false})); 
 
 app.get('/', function(req , res){
-    res.send('Working..');
+    couchdb.get(dbName , viewUrl).then(function(data , header , status){
+        console.log(data.data.rows);
+        res.render('index', {
+            customers : data.data.rows
+        })
+    }, function(err){
+        res.send(err)
+    });
+
+});
+
+app.post('/customer/add' , function(req , res){
+    const name = req.body.name;
+    const email = req.body.email;
+    couchdb.uniqid().then(function(ids){
+        const id = ids[0];
+
+        couchdb.insert('customers' , {
+            _id : id,
+            name: name , 
+            email : email,
+            
+        }).then(
+            function(data , header , status){
+                res.redirect('/');
+            },function(err){
+                res.send(err); 
+            }
+        )
+    });
+});
+
+app.post('/customer/delete/:id', function(req ,res){
+    const id = req.params.id;
+    const rev = req.body.rev;
+
+    couchdb.del(dbName , id ,rev).then(
+        function(data , headers , status){
+            res.redirect('/');
+        },function(){
+            res.send(err);
+        });
 });
 
 app.listen(3000 , function(){
